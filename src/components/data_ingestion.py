d4aa23c160f38fffaa49cbf789268b28f908ad64
ARUNKUMARVASUDEVAN <oyeahitsarun@gmail.com>
2023-10-15 20:49:46 +0530

    data_ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import pymongo
+import csv 
+from src.logger import logging
+from src.exception import CustomException
+from src.utils import mongo_data
+import os
+import sys
+from dataclasses import dataclass
+from src.utils import unnecessary
+from sklearn.model_selection import train_test_split
+
+@dataclass
+class DataIngestionconfig:
+    train_data_path:str=os.path.join('artifacts','train.csv')
+    test_data_path:str=os.path.join('artifacts','test.csv')
+    raw_data_path:str=os.path.join('artifacts','raw.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionconfig()
+
+    def initiate_data_ingestion(self):
+        logging.info('Data ingestion starts')
+
+        try:
+            data=mongo_data('Insurance','Insurance_')
+            logging.info("Dataset readed")
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
+            df=data.fetch()
+            df=unnecessary(df)
+
+            X=df.drop(labels=['expenses'],axis=1)
+            y=df[['expenses']]
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False)
+
+            logging.info('Train test split')
+
+            X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.30,random_state=21)
+            logging.info(f'X_train DataFrame Head:\n{X_train.head().to_string()}' )
+
+            train_set=pd.concat([X_train,y_train],axis=1)            
+            test_set=pd.concat([X_test,y_test],axis=1)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+             
+            logging.info('Ingestion of data completed')
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        
+        except Exception as e:
+            logging.info('Exception occured at Data Ingestion')
+
+            raise CustomException(e,sys)
+        
+if __name__=='__main__':
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    
---
+++ src/utils.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+import pymongo
+import csv
+from src.exception import CustomException
+from src.logger import logging
+import sys
+import pickle
+from sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error
+
+class mongo_data:
+    def __init__(self,db_name,collection_name):
+        self.db_name=db_name
+        self.collection_name=collection_name
+    def fetch(self):
+        try:
+            logging.info('fetching data from Mongo DB has started')
+            client=pymongo.MongoClient("mongodb://localhost:27017/")
+            db=client[self.db_name]
+            collection=db[self.collection_name]
+            cursor=collection.find()
+            data_list=[]
+            for doc in cursor:
+                data_list.append(doc)
+            data=pd.DataFrame(data_list)
+
+            return data
+        except Exception as e:
+            logging.info("Error occured on Mongodb_data")
+            raise CustomException(e,sys)
+def unnecessary(data):
+    df=data.drop('_id',axis=1)
+    return df
+
+def model_evaluate(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+
+        for model_name,model in models.items():
+            model.fit(X_train,y_train)
+            y_pred=model.predict(X_test)
+            r_square=r2_score(y_test,y_pred)
+            mse=mean_squared_error(y_test,y_pred)*100
+            report[model_name]=r_square,mse
+        return report
+    except Exception as e:
+        logging.info('Exception occure during model Training')
+        raise CustomException(e,sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.info('Exception occured in load_object function ')
+        raise CustomException(e,sys)
